8efecc2bcdde81e2a332b9e8d60392bcbb3e4577
Francesco.dalascio <francesco.dalascio.2002@gmail.com>
2023-12-18 15:05:58 +0100

    inseriti in appvue header e main

---
+++ src/App.vue
@@ -0,0 +1,23 @@
+<template lang="">
+  
+  <HeaderYu/>
+  <MainYu/>
+</template>
+
+<script>
+
+import HeaderYu from './components/HeaderYu.vue';
+import MainYu from './components/MainYu.vue';
+export default {
+  name: 'App',
+  components:{
+    HeaderYu,
+    MainYu,
+    
+  }
+  
+}
+</script>
+<style lang="scss">
+    @use './styles/general.scss' as *;
+</style>
